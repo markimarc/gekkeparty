29e783e494293a83145bf098c654f1c987eb5ad5
Marc <marctijssen@air-van-marc.home>
2016-12-05 18:30:30 +0100

    Site added

---
+++ site/js/custom.js
@@ -0,0 +1,17 @@
+// Mobile Navigation
+
+	$(".button-collapse").sideNav();
+
+// Smooth Scrolling
+
+	$(document).ready(function(){
+	   $('.scrollspy').scrollSpy();
+	 });
+
+// Max length subdomain
+
+	$(document).ready(function() {
+	  $('input#sub_domain, textarea#textarea1').characterCounter();
+	});
+
+	
